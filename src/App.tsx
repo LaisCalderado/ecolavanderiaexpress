57bfeffbc9b7af7c9f4aea1b22122b8396ef3d93
Lais Calderaro <laiscalderaro06@gmail.com>
2025-06-23 17:11:52 -0400

    [REMOVE] Contacts footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Conheca from './components/Conheca';
 import Services from './components/Services';
-import Contact from './components/Contact';
 import Local from './components/Local';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
@@ -19,7 +18,6 @@ function App() {
             <Conheca/>
             <Local />
             <FAQ />
-            <Contact />
             <Footer />
         </div>
     );
