37494c378f9b80a6c421f04429284dce6686c521
Lais Calderaro <laiscalderaro06@gmail.com>
2025-06-23 17:09:53 -0400

    [FEAT] Questions and answer for FAQ

---
+++ src/components/data/faqData.ts
@@ -0,0 +1,34 @@
+export const faqData = [
+    {
+        question: "Como funciona?",
+        answer: "Você utilizar nossas máquinas é fácil! Separe suas roupas por cores e tipos de tecido, carregue as máquinas de lavar, escolha o ciclo desejado e insira o pagamento. Nossas máquinas dosam automaticamente amaciantes e sabão, seguindo as instruções exibidas nelas."
+    },
+    {
+        question: "É necessário trazer meu próprio sabão ou amaciante?",
+        answer: "Não é necessário. Nossas máquinas fornecem automaticamente os sabão, amaciantes e outros produtos necessários para a lavagem, incorporando-os ao processo sem que você precise trazê-los."
+    },
+    {
+        question: "Preciso agendar horário?",
+        answer: "Não! Nosso serviço é de autoatendimento. Você pode ir quando quiser, sem precisar agendar."
+    },
+    {
+        question: "Vocês funcionam aos finais de semana?",
+        answer: "Sim! Estamos abertos todos os dias da semana, inclusive sábados, domingos e feriados de 8:00 às 22:00."
+    },
+    {
+        question: "Aceitam quais formas de pagamento?",
+        answer: "Aceitamos cartões de débito, crédito e Pix."
+    },
+    {
+        question: "Qual a quantidade de roupas por lavagem?",
+        answer: "Possuímos um cesto como medida para a quantidade de roupas correta. Cada cesto comporta 10kg de roupas, aproximadamente 25 peças (molhadas)"
+    },
+    {
+        question: "O que fazer se uma máquina estiver ocupada?",
+        answer: "Deve verificar se tem outra máquina livre ou aguardar o término da mesma."
+    },
+    {
+        question: "O que acontece se eu deixar minhas roupas dentro da máquina?",
+        answer: "Ao término, suas roupas ficarão dentro das máquinas, e o próximo cliente pode removê-las e colocá-las no cesto desocupado da lavanderia."
+    }
+];
